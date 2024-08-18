414d49d70e1e5c7fa4d4dc32976d8361c1d9a0c1
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-08-18 19:54:00 +0545

    added the shad cn component and made reusable modal for showing player has won

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,19 @@
-import { Button } from "@/components/button";
-import { Board } from "@/components/ui/board";
-import { PlayerScoreCount } from "@/components/ui/player-score-count";
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { PlayerScoreCount } from "@/components/player-score-count";
+import { Board } from "@/modules/board";
 
 export default function Home() {
   return (
     <main className="h-screen w-screen flex items-center justify-center bg-white dark:bg-[#0F0F10]">
-      <section className="w-[448px] h-[496px] bg-white dark:bg-[#18181b] rounded-lg p-6 shadow-2xl shadow-primary flex flex-col gap-6">
+      <section className="w-[448px] h-[496px] bg-white dark:bg-[#18181b] rounded-lg p-6 shadow-2xl shadow-[#E11D48] flex flex-col gap-6">
         <PlayerScoreCount />
         <Board />
         <div className="flex items-center justify-center w-full">
-          <Button />
+          <Button variant="outline" size="lg">
+            Reset
+          </Button>
         </div>
       </section>
     </main>
---
+++ src/modules/board.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { squares } from "@/constants/square";
+import { IoMdClose } from "react-icons/io";
+import { Modal } from "@/components/modal";
+import { useState } from "react";
+
+const PlayerWonModal = ({
+  closePlayerWonModal,
+}: {
+  closePlayerWonModal: () => void;
+}) => {
+  return (
+    <div className="h-72 w-[550px] bg-white flex flex-col gap-4 p-4 rounded-lg">
+      <div className="flex items-center justify-between">
+        <h1>Game Over</h1>
+        <div
+          className="p-1 flex items-center justify-center rounded cursor-pointer hover:bg-slate-300"
+          onClick={closePlayerWonModal}
+        >
+          <IoMdClose className="text-2xl" />
+        </div>
+      </div>
+      <div className="flex justify-center items-center h-full">
+        Player 1 has won
+      </div>
+    </div>
+  );
+};
+
+const usePlayerWonModal = () => {
+  const [isPlayerWonModalOpen, setPlayerWonModalOpen] = useState(false);
+
+  const openPlayerWonModal = () => setPlayerWonModalOpen(true);
+  const closePlayerWonModal = () => setPlayerWonModalOpen(false);
+
+  return {
+    isPlayerWonModalOpen,
+    openPlayerWonModal,
+    closePlayerWonModal,
+    PlayerWonModalComponent: (
+      <Modal
+        isModalOpen={isPlayerWonModalOpen}
+        closeModal={closePlayerWonModal}
+      >
+        <PlayerWonModal closePlayerWonModal={closePlayerWonModal} />
+      </Modal>
+    ),
+  };
+};
+
+export const Board = () => {
+  const { PlayerWonModalComponent, isPlayerWonModalOpen, openPlayerWonModal } =
+    usePlayerWonModal();
+
+  return (
+    <div className="grid grid-cols-3 h-full gap-4">
+      {squares.map((square) => (
+        <div
+          key={square.id}
+          className="bg-primary rounded-md dark:bg-[#C9C9C7] flex items-center justify-center cursor-pointer"
+          onClick={openPlayerWonModal}
+        ></div>
+      ))}
+      {isPlayerWonModalOpen && PlayerWonModalComponent}
+    </div>
+  );
+};
