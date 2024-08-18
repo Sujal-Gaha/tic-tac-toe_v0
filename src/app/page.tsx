21a3cbb60abcfdd6b626d6c11c06420838d5617d
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-08-18 21:16:06 +0545

    made the navbar responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,7 @@ import { Navbar } from "@/components/navbar";
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen flex items-center justify-center bg-white dark:bg-[#0F0F10]">
-      <Navbar />
+    <main className="flex items-center justify-center bg-white dark:bg-[#0F0F10] mt-60">
       <section className="w-[448px] h-[496px] bg-white dark:bg-[#18181b] rounded-lg p-6 shadow-2xl shadow-[#E11D48] flex flex-col gap-6">
         <PlayerScoreCount />
         <Board />
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -6,9 +6,9 @@ import { Button } from "./ui/button";
 
 export const Navbar = () => {
   return (
-    <nav className="lg:fixed top-0 h-20 w-full bg-black flex items-center justify-between py-8 px-40 text-white">
+    <nav className="fixed top-0 h-20 w-full bg-black flex items-center justify-between py-8 px-2 lg:px-40 text-white">
       <h1 className="text-4xl font-bold">Sujal</h1>
-      <div className="flex items-center gap-6 text-white">
+      <div className="hidden sm:flex items-center gap-6 text-white">
         {iconLinks.map((link) => (
           <Link
             key={link.id}
