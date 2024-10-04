ebc0e9e49ae61e50200389eb0dd1151d1d265a30
Sujal-Gaha <suzalgahamagar@gmail.com>
2024-10-04 19:51:06 +0545

    added winning conditions

--- src/hooks/useBoardFeatures.tsx
+++ src/hooks/useBoardFeatures.tsx
@@ -7,8 +7,11 @@ export type TTurn = "player_one" | "player_two";
 export const useBoardFeatures = () => {
   const [turn, setTurn] = useState<TTurn>("player_one");
   const [tiles, setTiles] = useState<TTile[]>(tilesData);
+  const [selectedTileP1, setSelectedTileP1] = useState<number[]>([]);
+  const [selectedTileP2, setSelectedTileP2] = useState<number[]>([]);
 
   const isPlayerOneTurn = turn === "player_one";
+  const isPlayerTwoTurn = turn === "player_two";
 
   const resetBoardFn = () => {
     setTurn("player_one");
@@ -38,9 +41,24 @@ export const useBoardFeatures = () => {
     setTiles(updatedTiles);
   };
 
+  const setSelectedTile = (id: number) => {
+    if (isPlayerOneTurn) {
+      const updatedSelectedTileP1 = [...selectedTileP1];
+      updatedSelectedTileP1.push(id);
+      setSelectedTileP1(updatedSelectedTileP1);
+    }
+
+    if (isPlayerTwoTurn) {
+      const updatedSelectedTileP2 = [...selectedTileP2];
+      updatedSelectedTileP2.push(id);
+      setSelectedTileP2(updatedSelectedTileP2);
+    }
+  };
+
   const handleTileClickedFn = (id: number) => {
     toggleTurnFn();
     setTileSelectedBy(id);
+    setSelectedTile(id);
   };
 
   return {
